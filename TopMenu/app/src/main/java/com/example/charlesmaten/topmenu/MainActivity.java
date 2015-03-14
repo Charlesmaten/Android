78dc69000ac49944d3896a242e4bdfdd86bf410c
Charlesmaten <Eagle_day@hotmail.com>
2015-03-14 23:27:29 +0100

    topmenu tilføjet

---
+++ TopMenu/app/src/main/java/com/example/charlesmaten/topmenu/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.charlesmaten.topmenu;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.SearchView;
+//import android.widget.ShareActionProvider;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    private Button SearchButton;
+    private SearchView sv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.action_settings);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        mShareActionProvider.setShareIntent(getDefaultIntent());
+        return true;
+
+
+    }
+
+    private Intent getDefaultIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        return intent;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+
+
+
+
+
+        //return super.onOptionsItemSelected(item);
+
+
+       /*
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+        */
+    }
+
+
+
+    private void openSearch() {
+       // InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+       // imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+
+
+
+
+
+        //sv = (SearchView) findViewById(R.id.action_search);
+        //sv.setIconified(false);
+
+
+
+
+
+        //Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSettings(){
+        Toast.makeText(this, "Search button pressed", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+}
